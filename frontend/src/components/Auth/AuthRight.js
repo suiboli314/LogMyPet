0c6cdfda530a6379d5b521c6a8934f4dea1fcdce
picklerick94 <jastangting@gmail.com>
2022-11-13 12:51:17 -0800

    login ft updated

--- frontend/src/components/Auth/AuthRight.js
+++ frontend/src/components/Auth/AuthRight.js
@@ -2,11 +2,16 @@ import React from "react";
 import "../../assets/styles/Auth.css";
 
 const AuthRight = () => {
-
   return (
     <div className="auth-right-bg">
-      <div>Record every</div>
-      <div>moement of your pet</div>
+      <div className="auth-right-headline">
+        <div>Record every</div>
+        <div>moment of your pet</div>
+      </div>
+      <img
+        className="auth-right-pic"
+        src="https://assets.moveshanghai.com/Open%20Doodles%20-%20Playing.png"
+      ></img>
     </div>
   );
 };

6c382a540b56249457353ec03b877c9a5ae6fc18
picklerick94 <jastangting@gmail.com>
2022-11-27 23:26:55 -0800

    updated create records

--- db/index.js
+++ db/index.js
@@ -3,10 +3,8 @@ import config from "../config.js";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 
-// const mongoURL = config.MONGO_URL || "mongodb://localhost:27017";
-const mongoURL =
-  config.MONGO_URL ||
-  "mongodb+srv://dylantse:IHcuwrJ9F648zvYH@cluster0.vrljs4f.mongodb.net/?retryWrites=true&w=majority";
+const mongoURL = config.MONGO_URL || "mongodb://localhost:27017";
+
 const DB_NAME = "logMyPetDB";
 const PET_COLLECTION_NAME = "pets";
 const USER_COLLECTION_NAME = "users";
